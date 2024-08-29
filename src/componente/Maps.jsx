a28cad00714ed8cb89eaa340398fcafd9638a297
Axel <axelgaum@gmail.com>
2024-08-29 20:48:54 -0300

    los mapas mentales se muetran

--- src/componente/Maps.jsx
+++ src/componente/Maps.jsx
@@ -35,34 +35,40 @@ useEffect(() => {
   fetchMapas();
 }, []);
 
-
-  return (
-    <div className='container mx-auto p-6'>
+return (
+  <div className='container mx-auto p-6'>
     <h2 className='text-2xl font-semibold mb-4'>Todos los Mapas</h2>
-    {mapas.length > 0 ? (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-      {mapas.map((mapa) => (
-        <div key={mapa._id} className='bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200'>
-          <div className='p-4'>
-            <h3 className='text-lg font-semibold mb-2'>{mapa.nombre}</h3>
-            <img src={mapa.imagen} alt={mapa.nombre} className='w-full h-auto object-cover mb-4 rounded-lg' style={{ maxHeight: '150px' }} />
-            <p className='text-gray-600'>Miembros: {mapa.miembros}</p>
-            <p className='text-gray-600'>Modificado: {mapa.modificado}</p>
+    {loading ? (
+      <p>cargando mapas...</p>
+    ) : mapas.length > 0 ? (
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+        {mapas.map((mapa) => (
+          <div key={mapa._id} className='bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300'>
+            <div className='p-4'>
+              <h3 className='text-lg font-semibold mb-2'>{mapa.title}</h3>
+              <img
+                src={mapa.thumbnail}
+                alt={mapa.title}
+                className='w-full h-auto object-cover mb-4 rounded-lg'
+                style={{ maxHeight: '250px', objectFit: 'cover' }}
+              />
+              <p className='text-gray-600'>Miembro: {mapa.user}</p>
+              <p className='text-gray-600'>Creado: {new Date(mapa.updatedAt).toLocaleDateString()}</p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  ) : (
-    <div className="text-center p-6">
-          <p className="text-gray-600 text-lg mb-4">No tienes mapas mentales creados aún.</p>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-            Crear nuevo mapa mental
-          </button>
-        </div>
-      )}
+        ))}
+      </div>
+    ) : (
+      <div className="text-center p-6">
+        <p className="text-gray-600 text-lg mb-4">No tienes mapas mentales creados aún.</p>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+          Crear nuevo mapa mental
+        </button>
+      </div>
+    )}
   </div>
-
 );
+
 }
 
 export default Maps
