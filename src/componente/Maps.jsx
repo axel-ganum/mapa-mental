187af0850349bc20b3b9c88728e32c15d9f55dd9
Axel <axelgaum@gmail.com>
2024-08-29 13:17:21 -0300

    Se realizaron cambios en el frontend

--- src/componente/Maps.jsx
+++ src/componente/Maps.jsx
@@ -1,42 +1,63 @@
-import React from 'react'
-import foto from '../assets/mapa.jpg'
+import React, { useEffect, useState } from 'react' 
+
 
 const Maps = () => {
-  const mapas = [
-    {
-      id: 1,
-      nombre: 'Mi Nuevo Mapa Mental',
-      miembros: 'Solo tú',
-      modificado: '1 de jun. de 2024',
-      imagen: foto// URL de la miniatura
-    },
-    {
-      id: 2,
-      nombre: 'Mi Nuevo Mapa Mental',
-      miembros: 'Solo tú',
-      modificado: '2 de jun. de 2024',
-      imagen: foto// URL de la miniatura
-    },
-    {
-      id: 3,
-      nombre: 'Mi Nuevo Mapa Mental',
-      miembros: 'Solo tú',
-      modificado: '4 de jun. de 2024',
-      imagen: foto// URL de la miniatura
-    },
-  ];
+const [mapas, setMapas] = useState([]);
+const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+  const fetchMapas = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const response = await fetch('http://localhost:3000/maps/all',{
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`, 
+          'Content-Type': 'application/json'
+        }
+      });
+
+       if (!response.ok) {
+        throw new Error('Error al obtener los mapas mentales')
+       }
+
+
+      const data = await response.json();
+
+      setMapas(data)
+    } catch (error) {
+      console.error('Error al obtener los mpas mentales:', error)
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  fetchMapas();
+}, []);
+
+
   return (
     <div className='container mx-auto p-6'>
     <h2 className='text-2xl font-semibold mb-4'>Todos los Mapas</h2>
-    {mapas.length > 0 ? (
+    { loading ? (
+      <p>cargando mapas...</p>
+    ):
+   mapas.length > 0 ? (
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
       {mapas.map((mapa) => (
-        <div key={mapa.id} className='bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200'>
+        <div key={mapa._id} className='bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200'>
           <div className='p-4'>
-            <h3 className='text-lg font-semibold mb-2'>{mapa.nombre}</h3>
-            <img src={mapa.imagen} alt={mapa.nombre} className='w-full h-auto object-cover mb-4 rounded-lg' style={{ maxHeight: '150px' }} />
-            <p className='text-gray-600'>Miembros: {mapa.miembros}</p>
-            <p className='text-gray-600'>Modificado: {mapa.modificado}</p>
+            <h3 className='text-lg font-semibold mb-2'>{mapa.title}</h3>
+           {mapa.thumbnail ? (
+            <img src={mapa.thumbnail.startsWith('data:image') ? mapa.thumbnail : `data:image/png;base64,${mapa.thumbnail}`}
+            alt={mapa.title} 
+            className='w-full h-auto object-cover mb-4 rounded-lg'
+             />
+           ) : ( 
+             <p className='text-gray-500'>No hay imagen disponible</p>
+           )}
+            <p className='text-gray-600'>Miembro: {mapa.usernama}</p>
+            <p className='text-gray-600'>Creado:{new Date(mapa.updatedAt).toLocaleDateString()}</p>
           </div>
         </div>
       ))}
