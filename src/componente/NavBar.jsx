c83a9fa93a420509878b062c9a2fa88b6ae8c002
Axel <axelgaum@gmail.com>
2024-10-05 21:15:55 -0300

    se realizo par las notificaciones

--- src/componente/NavBar.jsx
+++ src/componente/NavBar.jsx
@@ -1,85 +1,167 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Notifications from '../componente/Notifications.jsx';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 const NavBar = () => {
   const [isOpen, setIopen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [unreadCount, setUnreadCount] = useState(0);
 
   const toggleMenu = () => {
-    setIopen(!isOpen)
+    setIopen(!isOpen);
+  };
+
+  const toggleNotificationsModal = () => {
+    setIsModalOpen(!isModalOpen);
   }
+
+   
+     const fetchUnreadNotifications = async () => {
+      try {
+      const token = localStorage.getItem('token');
+      const response = await fetch('http://localhost:3000/unread', {
+        method : 'GET',
+        headers : {
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+     if(!response.ok) {
+      throw new Error('Error al obtener el numero de notificaciones no leidas')
+     }
+      const data = await response.json();
+      setUnreadCount(data.unreadCount)
+      
+  }catch (error) {
+    console.error('Error fetching unread notifications:', error)
+  }
+};
+     
+
+  useEffect(() => {
+    fetchUnreadNotifications();
+    const interval = setInterval(() => {
+      fetchUnreadNotifications();
+    })
+   return () => clearInterval(interval)
+  },[])
+
+  const handleNotificationRead = async (notificationId) => {
+    try {
+      const token = localStorage.getItem('token');
+       
+      await fetch(`http://localhost:3000/notifications/${notificationId}`, {
+        method : 'PUT',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+      
+      fetchUnreadNotifications()
+    } catch (error) {
+      console.error('Error al marcar la notificacion como leida', error)
+    }
+  };
+
+
   return (
-    <div className='bg-black p-4'>  
-    <nav className='container mx-auto flex items-center justify-between'>
-    <div  className= ' text-white  text-lg font-bold'>
-      Mi Mapa Mental 
-      </div>
-      <div className='block lg:hidden'>
-        <button 
-         onClick={toggleMenu}
-         className='text-white focus:outline-none focus:text-white'
-         >
-
-          <svg 
-              className='w-g h-6'
-              fill='none'
-              stroke='currentColor'
-              viewBox='0 0 24 24'
-              xmlns='http://www.w3.org/2000/sv'
-              >
-                <path 
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16m-7 6h7'}
-            ></path>
-              </svg>
-
-         </button>
-
-
-      </div>
-      <div className= {`lg:flex ${isOpen ? 'block' : 'hidden'} mt-4 lg:mt-0`}>
-       <ul className='flex space-x-7 text-white'> 
-         <li><Link
-          to="/"
-           className='hover:text-gray-300'
-           onClick={() => setIopen(false)}
-           >Home</Link></li>
-
-         <li><Link
-          to="/profile"
-           className='hover:text-gray-300'
-           onClick={() => setIopen(false)} 
-           >Perfil</Link></li>
-           <li>
-            <Link 
-            to= '/login'
-            className='hover:text-gray-300'
-            onClick={() => setIopen(false)}
+    <div className="bg-black p-4">
+      <nav className="container mx-auto flex items-center justify-between">
+        <div className=" text-white  text-lg font-bold">Mi Mapa Mental</div>
+        <div className="block lg:hidden">
+          <button
+            onClick={toggleMenu}
+            className="text-white focus:outline-none focus:text-white"
+          >
+            <svg
+              className="w-g h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/sv"
             >
-               Iniciar Sesión
-            </Link>
-           </li>
-           <li>
-            <Link
-            to='/register'
-            className='hover:text-gray-300'
-            onClick={() => setIopen(false)}>
-              Registrarse
-            </Link>
-           </li>
-         <li> 
-          
-          <Link
-          to="/logout" 
-          className='hover:text-gray-300'
-          onClick={() => setIopen(false)} 
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div className={`lg:flex ${isOpen ? "block" : "hidden"} mt-4 lg:mt-0`}>
+          <ul className="flex space-x-7 text-white">
+            <li>
+              <Link
+                to="/"
+                className="hover:text-gray-300"
+                onClick={() => setIopen(false)}
+              >
+                Home
+              </Link>
+            </li>
 
-          >Salir</Link></li>
-       </ul>
-       </div>
-    </nav>
+            <li>
+              <Link
+                to="/profile"
+                className="hover:text-gray-300"
+                onClick={() => setIopen(false)}
+              >
+                Perfil
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/login"
+                className="hover:text-gray-300"
+                onClick={() => setIopen(false)}
+              >
+                Iniciar Sesión
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/register"
+                className="hover:text-gray-300"
+                onClick={() => setIopen(false)}
+              >
+                Registrarse
+              </Link>
+            </li>
+            <li>
+               <button onClick={toggleNotificationsModal} className="relative">
+                 <i className="fas fa-bell text-white"></i> 
+                  {unreadCount > 0 && (
+                   <span className="absolute top-0 right-0 bg-red-500 text-white rounded-full px-2 text-xs">
+                      {unreadCount}
+                   </span>
+                    )}
+               </button>
+            </li>
+            <li>
+              <Link
+                to="/logout"
+                className="hover:text-gray-300"
+                onClick={() => setIopen(false)}
+              >
+                Salir
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+       <Notifications
+        isModalOpen ={isModalOpen} 
+        onClose = {() => {
+          setIsModalOpen(false);
+          handleNotificationRead();
+        }}
+        
+       />
     </div>
-  )
-}
+ );
+};
 
-export default NavBar
+export default NavBar;
--- src/componente/Notifications.jsx
+++ src/componente/Notifications.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { type } from 'os';
 
-const Notifications = ({isOpen, onClose}) => {
+const Notifications = ({isModalOpen, onClose}) => {
     const [notifications, setNotifications] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [ws, setWs] = useState(null);
 
     useEffect(() => {
       const fetchNotifications = async () => {
         try {
             const token = localStorage.getItem('token');
-            const response = fetch('http://localhost:3000/notifications', {
+            const response = await fetch('http://localhost:3000/notifications', {
                 method: 'GET',
                     headers: {
                         'Authorization': `Bearer ${token}`,
@@ -30,10 +33,44 @@ const Notifications = ({isOpen, onClose}) => {
             setLoading(false)
         }
       }
-     if(isOpen){
+     if(isModalOpen){
       fetchNotifications();
+      setUpWebSocekt();
+      
      }
-    },[isOpen])
+
+       return () => {
+        if(ws) {
+          ws.close();
+        }
+       }
+
+    },[isModalOpen])
+
+    const setUpWebSocekt = () => {
+      const token = localStorage.getItem('token');
+      
+      const webSocket = new WebSocket(`ws://localhost:3000/ws?token=${token}`);
+
+      webSocket.onmessage = (event) => {
+        const message = JSON.parse(event.data);
+        if (message.type === 'new_notification') {
+          setNotifications((preventNotificacions) => [message.notification, ...preventNotificacions]);
+
+        }else if (message.type === 'mark_as_read') {
+
+          setNotifications((prevNotifications) => 
+             prevNotifications.map((n) => 
+                 n._id === message.notificationId ? {...n, read: true} : n
+            )
+          );
+
+        }
+      };
+
+       setWs(webSocket);
+
+    }
 
      const hanldeNotificationClick = async (notificationId) => {
       try {
@@ -46,18 +83,26 @@ const Notifications = ({isOpen, onClose}) => {
           }
         });
 
+        if (ws) {
+           ws.send(JSON.stringify({type:'mark-as-read', notificationId}))
+        }
+
         setNotifications(prevNotifications => prevNotifications.filter(n => n._id !== notificationId ))
       } catch (error) {
          console.error('Error al marcar como leido', error)
       }
      };
 
-     if(!isOpen) return null
+     if(!isModalOpen) return null
 
   return (
-    <div className='fixed inset-0 flex items-center justify-center z-50'>
-      <div className='bg-white p-6 rounded shadow-lg w-96'>
+      <div className='absolute right-0 mt-2  bg-white p-6 rounded shadow-lg z-50 w-96 '>
+        <div className='flex justify-between items-center mb-4'>
         <h2 className='text-xl font-semibold mb-4'>Notificaciones</h2>
+        <button onClick={onClose} className='text-gray-500 hover:text-gray-700'>
+          <i className='fas fa-times'></i>
+        </button>
+        </div>
         <ul>
            {notifications.length > 0 ? (
               notifications.map((notification) => (
@@ -73,12 +118,10 @@ const Notifications = ({isOpen, onClose}) => {
               <p>No hay notificaciones</p>
            )}
         </ul>
-        <button onClick={onClose} className='mt-4 bg-blue-500 text-white px-4 py-4 rounded'>
-          Cerrar
-        </button>
+        
       </div>
       
-    </div>
+   
   )
 }
 
