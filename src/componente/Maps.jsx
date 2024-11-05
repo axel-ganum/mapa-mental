def44e6697a4de80c66069f0fa733738f2582028
Axel <axelgaum@gmail.com>
2024-11-04 21:44:23 -0300

    se realizo la parte para que solo el dueño pueda ver el boton

--- src/componente/Maps.jsx
+++ src/componente/Maps.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react' 
-import { useNavigate } from 'react-router-dom'
+import { useNavigate,useParams } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import {Link} from 'react-router-dom';
 
 const Maps = () => {
 const [mapas, setMapas] = useState([]);
 const [loading, setLoading] = useState(true);
+const {userId} = useParams();
 const navigate = useNavigate();
 
 useEffect(() => {
@@ -68,103 +69,111 @@ const handleViewMore = (mapa_id) => {
     toast.error('Error al eliminar el mapa mental')
   }
  }
-  return (
-    <div className="container mx-auto p-6">
-      <ToastContainer/>
-  <h2 className="text-2xl font-semibold mb-4">Todos los Mapas</h2>
-  {loading ? (
-    <p>cargando mapas...</p>
-  ) : mapas.length > 0 ? (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {mapas.map((mapa) => {
-        // Establece una altura fija para la imagen
-        const imageHeight = 200; // Ajusta la altura fija que desees
-        const aspectRatio = 16 / 9;
-        return (
-          <div
-            key={mapa._id}
-            className="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200 flex flex-col"
-          >
-            <div className="p-4 flex-grow flex flex-col">
-              <h3 className="text-lg font-semibold mb-2">{mapa.title}</h3>
 
-              {mapa.sharedWith && mapa.sharedWith.includes(userId) && (
-                <div className='text-sm text-gray-500 italic mb-2'>
-                  Compartido contigo
-                  <div className='mt-1'>
-                  <p className='font-semibold'>Compartir con:</p>
-                  <ul className='list-disc list-inside'>
-                    {mapa.sharedWith.map((user) => (
-                      <li key={user._id} className='text-gray-700'>
-                        {user.username}
-                      </li>
-                    ))}
-                  </ul>
-              </div>
-              </div>
-                
-              )}
-              <div
-                className="relative w-full"
-                style={{ height: `${imageHeight}px`, paddingBottom: `${100 / aspectRatio}%`, overflow: 'hidden' }}
-              >
-                {mapa.thumbnail ? (
-                  <img
-                    src={
-                      mapa.thumbnail.startsWith("data:image")
-                        ? mapa.thumbnail
-                        : `data:image/png;base64,${mapa.thumbnail}`
-                    }
-                    alt={mapa.title}
-                    className="absolute inset-0 w-full h-full object-cover"
-                    style={{
-                      filter: "contrast(0.8)",
-                      boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-                    }}
-                  />
-                ) : (
-                  <p className="text-gray-500">No hay imagen disponible</p>
+ console.log("userId:", userId);
+ console.log(mapas)
+
+ return (
+  <div className="container mx-auto p-6">
+    <ToastContainer />
+    <h2 className="text-2xl font-semibold mb-4">Todos los Mapas</h2>
+    {loading ? (
+      <p>Cargando mapas...</p>
+    ) : mapas.length > 0 ? (
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {mapas.map((mapa) => {
+          const imageHeight = 200; // Ajusta la altura fija que desees
+          const aspectRatio = 16 / 9;
+          return (
+            <div
+              key={mapa._id}
+              className="bg-white rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 flex flex-col border border-gray-200"
+            >
+              <div className="p-6 flex-grow flex flex-col">
+                <h3 className="text-lg font-semibold mb-2">{mapa.title}</h3>
+
+                {mapa.sharedWith && mapa.sharedWith.includes(user._Id) && (
+                  <div className='text-sm text-gray-500 italic mb-2'>
+                    Compartido contigo
+                    <div className='mt-1'>
+                      <p className='font-semibold'>Compartir con:</p>
+                      <ul className='list-disc list-inside'>
+                        {mapa.sharedWith.map((user) => (
+                          <li key={user._id} className='text-gray-700'>
+                            {user.username}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
                 )}
+                <div
+                  className="relative w-full"
+                  style={{ height: `${imageHeight}px`, paddingBottom: `${100 / aspectRatio}%`, overflow: 'hidden' }}
+                >
+                  {mapa.thumbnail ? (
+                    <img
+                      src={
+                        mapa.thumbnail.startsWith("data:image")
+                          ? mapa.thumbnail
+                          : `data:image/png;base64,${mapa.thumbnail}`
+                      }
+                      alt={mapa.title}
+                      className="absolute inset-0 w-full h-full object-cover"
+                      style={{
+                        filter: "contrast(0.8)",
+                        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+                      }}
+                    />
+                  ) : (
+                    <p className="text-gray-500">No hay imagen disponible</p>
+                  )}
+                </div>
+                <div className="mt-4 flex-grow">
+                  <p className="text-gray-600">Miembro: {mapa.user.username}</p>
+                  <p className="text-gray-600">Descripción: {mapa.description}</p>
+                  <p className="text-gray-600">
+                    Creado: {new Date(mapa.createdAt).toLocaleDateString()}
+                  </p>
+                  <p className="text-gray-600">
+                    Editado: {new Date(mapa.updatedAt).toLocaleDateString()}
+                  </p>
+                </div>
               </div>
-              <div className="mt-4 flex-grow">
-                <p className="text-gray-600">Miembro: {mapa.user.username}</p>
-                <p className="text-gray-600">Descripcion: {mapa.description}</p>
-                <p className="text-gray-600">
-                  Creado: {new Date(mapa.updatedAt).toLocaleDateString()}
-                </p>
+              <div className='p-4 flex justify-end'>
+                <button 
+                  onClick={() => handleViewMore(mapa._id)}
+                  className='bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition-colors duration-200 mr-4 shadow-md hover:shadow-lg'
+                >
+                  Ver más
+                  
+                </button>
+                 {mapa.isOwner && 
+                <button 
+                  onClick={() => handleDeleteMap(mapa._id)}
+                  className='bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition-colors duration-200 shadow-md hover:shadow-lg'
+                >
+                  Eliminar
+                </button>
+                }
               </div>
             </div>
-            <div className='p-4 flex justify-end'>
-               <button 
-               onClick={() => handleViewMore(mapa._id)}
-               className='bg-blue-500 text-white px-4 rounded hover:bg-blue-600 transition-transform hover:scale-105 mr-4'>
-                Ver más
-               </button>
-               <button 
-                onClick={() => handleDeleteMap(mapa._id)}
-                className='bg-red-500 text-white px-4 rounded hover:bg-red-600 transition-transform hover:scale-105'
-               >
-                Eliminar
-               </button>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  ) : (
-    <div className="text-center p-6">
-      <p className="text-gray-600 text-lg mb-4">
-        No tienes mapas mentales creados aún.
-      </p>
-      <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-        Crear nuevo mapa mental
-      </button>
-    </div>
-  )}
-</div>
-
-
+          );
+        })}
+      </div>
+    ) : (
+      <div className="text-center p-6">
+        <p className="text-gray-600 text-lg mb-4">
+          No tienes mapas mentales creados aún.
+        </p>
+        <Link to="/editor" className="px-6 py-2 text-white bg-blue-500 hover:bg-blue-700 rounded-lg transition-transform transform hover:scale-105 shadow-md hover:shadow-lg">
+          Crear Nuevo Mapa
+        </Link>
+      </div>
+    )}
+  </div>
 );
+
 }
 
-export default Maps
+export default Maps;
--- src/componente/Profile.jsx
+++ src/componente/Profile.jsx
@@ -1,21 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+
 
 const Profile = () => {
   const [isEditing, setIsEditing] = useState(false);
   const [showFullImage, setShowFullImage] = useState(false);
-  const [user, setUser] = useState({
-    name: 'Ramdom',
-    email: 'ramdom@example.com',
-    theme: 'Claro',
-    language: 'Español',
-    profilePic: '', // Inicialmente no hay imagen de perfil
-    stats: {
-      totalMapas: 10,
-      sharesMpas: 5,
-      activeCollaborations: 2,
-      lastActivity: '15 de jun. de 2024'
+  const [user, setUser] = useState({});
+  const [isSaved, setIsSaved] = useState(false)
+
+  useEffect(() => {
+   const fecthUserProfile = async () => {
+     try {
+      const token = localStorage.getItem('token')
+      const response = await fetch('http://localhost:3000/perfil', {
+        method: 'GET',
+        headers: {
+        'Authorization': `Bearer ${token}`,  
+        'Content-Type': 'application/json',
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error('Error al obtener el perfil del usuario')
+        
+      }
+
+      const data = await response.json();
+      setUser(data)
+      
+    } catch (error) {
+       console.error('Error al obtener el perfil del usuario:', error)
+      
     }
-  });
+   }
+ {
+    fecthUserProfile()
+}
+
+  }, [])
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -33,68 +54,81 @@ const Profile = () => {
     }
   };
 
+  const updateUserProfile = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const response = await fetch('http://localhost:3000/perfil', {
+        method: 'PUT',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+         body: JSON.stringify({
+          theme: user.theme,
+          profilePicture: user.profilePicture,
+         }),
+      });
+
+      if (!response.ok) {
+         throw new Error('Error al actualilzar el perfil')
+      }
+
+      const data = await response.json();
+      console.log(data.message);
+      setUser(data.user)
+      setIsEditing(false)
+    } catch (error) {
+      console.error('Error al actualizar el perfil:', error)
+    }
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('Perfil actualizado:', user);
-    setIsEditing(false);
+    setIsSaved(true);
+    setTimeout(() => setIsSaved(false), 2000);
+    updateUserProfile();
   };
 
-  return (
+   const handleLogout = () => {
+
+    localStorage.removeItem('token');
+
+    window.location.href = "/login"
+   }
+   return (
     <div className="container mx-auto p-6">
-      <h2 className="text-2xl font-semibold mb-4">Perfil</h2>
+      <h2 className="text-3xl font-semibold mb-6 text-gray-800">Perfil</h2>
+      
       <div className="bg-white p-6 rounded-lg shadow-lg">
         {isEditing ? (
-          <form onSubmit={handleSubmit} className="bg-gray-100 p-6 rounded-lg shadow-lg">
-            <h4 className="text-lg font-semibold mb-4">Editar Perfil</h4>
+          <form onSubmit={handleSubmit} className="bg-gray-50 p-6 rounded-lg shadow-md">
+            <h4 className="text-lg font-semibold mb-4 text-gray-700">Editar Perfil</h4>
             <div className="mb-4">
-              <label className="block text-gray-700">Nombre:</label>
+              <label className="block text-gray-600 mb-1">Nombre:</label>
               <input
                 type="text"
                 name="name"
-                value={user.name}
+                value={user.username}
                 onChange={handleChange}
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
               />
             </div>
             <div className="mb-4">
-              <label className="block text-gray-700">Correo Electrónico:</label>
+              <label className="block text-gray-600 mb-1">Correo Electrónico:</label>
               <input
                 type="email"
                 name="email"
                 value={user.email}
                 onChange={handleChange}
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
               />
             </div>
             <div className="mb-4">
-              <label className="block text-gray-700">Tema:</label>
-              <select
-                name="theme"
-                value={user.theme}
-                onChange={handleChange}
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-              >
-                <option value="Claro">Claro</option>
-                <option value="Oscuro">Oscuro</option>
-              </select>
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700">Idioma:</label>
-              <select
-                name="language"
-                value={user.language}
-                onChange={handleChange}
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-              >
-                <option value="Español">Español</option>
-                <option value="Inglés">Inglés</option>
-              </select>
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700">Foto de Perfil:</label>
+              <label className="block text-gray-600 mb-1">Foto de Perfil:</label>
               <div className="relative group">
                 <img
-                  src={user.profilePic || 'https://via.placeholder.com/150'}
+                  src={user.profilePicture || 'https://via.placeholder.com/150'}
                   alt="Profile"
                   className="w-32 h-32 rounded-full mx-auto mb-2 cursor-pointer"
                   onClick={() => setShowFullImage(true)}
@@ -102,7 +136,7 @@ const Profile = () => {
                 <button
                   type="button"
                   onClick={() => document.getElementById('fileInputForm').click()}
-                  className="absolute bottom-0 right-0 bg-blue-500 text-white p-1 rounded-full"
+                  className="absolute bottom-0 right-0 bg-blue-500 text-white p-1 rounded-full shadow-md transition hover:bg-blue-600"
                 >
                   ✎
                 </button>
@@ -115,17 +149,17 @@ const Profile = () => {
                 />
               </div>
             </div>
-            <div className="flex justify-between">
+            <div className="flex justify-between mt-6">
               <button
                 type="submit"
-                className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+                className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition"
               >
-                Guardar Cambios
+              {isSaved? "Guardando!" : "Guardar Cambios"}
               </button>
               <button
                 type="button"
                 onClick={() => setIsEditing(false)}
-                className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600"
+                className="bg-gray-400 text-white px-4 py-2 rounded-lg hover:bg-gray-500 transition"
               >
                 Cancelar
               </button>
@@ -133,21 +167,15 @@ const Profile = () => {
           </form>
         ) : (
           <div>
-            <div className="flex items-center mb-4">
+            <div className="flex items-center mb-6">
+            <h3 className="text-2xl font-semibold text-gray-700">{user.username}</h3>
               <div className="relative group">
                 <img
-                  src={user.profilePic || 'https://via.placeholder.com/150'}
+                  src={user.profilePicture || 'https://via.placeholder.com/150'}
                   alt="Profile"
-                  className="w-16 h-16 rounded-full mr-4 cursor-pointer"
+                  className="w-16 h-16 rounded-full mr-4 cursor-pointer shadow-md"
                   onClick={() => setShowFullImage(true)}
                 />
-                <button
-                  type="button"
-                  onClick={() => document.getElementById('fileInput').click()}
-                  className="absolute bottom-0 right-0 bg-blue-500 text-white p-1 rounded-full"
-                >
-                  ✎
-                </button>
                 <input
                   type="file"
                   accept="image/*"
@@ -157,45 +185,40 @@ const Profile = () => {
                 />
               </div>
               <div>
-                <h3 className="text-xl font-bold">{user.name}</h3>
-                <p className="text-gray-600">{user.email}</p>
+                <h3 className="text-2xl font-semibold text-gray-700">{user.name}</h3>
+                <p className="text-gray-500">{user.email}</p>
               </div>
             </div>
-
-            <div className="mb-4">
-              <h4 className="text-lg font-semibold mb-2">Preferencias</h4>
-              <p className="text-gray-600">Tema: {user.theme}</p>
-              <p className="text-gray-600">Idioma: {user.language}</p>
+            <div className="mb-6">
+              <h4 className="text-lg font-semibold mb-2 text-gray-700">Estadísticas</h4>
+              <p className="text-gray-600">Total de Mapas: {user.stats?.totalMapas || 0}</p>
+              <p className="text-gray-600">Mapas Compartidos: {user.stats?.sharesMpas || 0}</p>
+              <p className="text-gray-600">Colaboraciones Activas: {user.stats?.activeCollaborations || 0}</p>
             </div>
-
-            <div className="mb-4">
-              <h4 className="text-lg font-semibold mb-2">Estadísticas</h4>
-              <p className="text-gray-600">Total de Mapas: {user.stats.totalMapas}</p>
-              <p className="text-gray-600">Mapas Compartidos: {user.stats.sharesMpas}</p>
-              <p className="text-gray-600">Colaboraciones Activas: {user.stats.activeCollaborations}</p>
-              <p className="text-gray-600">Última Actividad: {user.stats.lastActivity}</p>
-            </div>
-
-            <div className="mt-6">
+  
+            <div className="mt-6 flex space-x-4">
               <button
                 type="button"
                 onClick={() => setIsEditing(true)}
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mr-2"
+                className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition"
               >
                 Editar Perfil
               </button>
-              <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
+              <button
+                className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition"
+                onClick={handleLogout}
+              >
                 Cerrar Sesión
               </button>
             </div>
           </div>
         )}
       </div>
-
+  
       {showFullImage && (
         <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
           <img
-            src={user.profilePic}
+            src={user.profilePicture}
             alt="Profile"
             className="w-auto h-auto max-w-full max-h-full"
             onClick={() => setShowFullImage(false)}
@@ -204,8 +227,7 @@ const Profile = () => {
       )}
     </div>
   );
-}
-
+}  
 export default Profile;
 
 
