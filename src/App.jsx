b72d503bc804bf57516f8db3260b669454cc7559
axel-ganum <axel.gaum@gmail.com>
2025-12-21 13:09:48 -0300

    Fix: Solucionados errores de construcción (CSS syntax e importación de reactflow)

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Home from './componente/Home';
 import Profile from './componente/Profile';
 import Maps from './componente/Maps';
 import MapaEditor from './componente/MapaEditor';
-import { ReactFlowProvider } from 'react-flow-renderer'
+import { ReactFlowProvider } from 'reactflow';
+import 'reactflow/dist/style.css';
 import LoginForm from './componente/LoginForm';
 import RegisterForm from './componente/RegisterForm';
 import ProtectedRoute from './componente/ProtectedRoute';
@@ -15,23 +16,23 @@ import { WebSocketProvider } from './componente/context/WebSocjetContext';
 const App = () => {
   return (
     <Router>
-      
+
       <div>
-      <WebSocketProvider>  
-      <NavBar/>
-      </WebSocketProvider>
-       <Routes>     
-       <Route path="/login" element={<LoginForm />} />
+        <WebSocketProvider>
+          <NavBar />
+        </WebSocketProvider>
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<ProtectedRoute element={<Profile />} />} />
-        <Route path="/editor" element={<ProtectedRoute element={<MapaEditor />} />} />
-        <Route path="/maps" element={<ProtectedRoute element={<Maps />} />} />
-      <Route path='/logout' element={<Logout/>} />
-      <Route path='/compartir/:mapId' element={<CompartirMapa/>} />
+          <Route path="/editor" element={<ProtectedRoute element={<MapaEditor />} />} />
+          <Route path="/maps" element={<ProtectedRoute element={<Maps />} />} />
+          <Route path='/logout' element={<Logout />} />
+          <Route path='/compartir/:mapId' element={<CompartirMapa />} />
         </Routes>
       </div>
-    
+
 
     </Router>
   )
