4ca7d5a2ee022e6f4dc1afa29375ea598d07601f
axel-ganum <axel.gaum@gmail.com>
2025-12-22 11:13:18 -0300

    Fix: Definitive CSS purge and restoration of src/index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './styles-v3.css'
+import './index.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
